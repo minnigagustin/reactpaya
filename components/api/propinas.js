cca789a9b60fdf672129d053e49e6b6bce017848
Julio Truzzi <julio.truzzi@gmail.com>
2021-12-13 23:07:21 -0300

    env var

--- components/api/propinas.js
+++ components/api/propinas.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000/api/propinas";
+const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api/propinas`;
 
 const createAxios = () => {
   axios.create();
